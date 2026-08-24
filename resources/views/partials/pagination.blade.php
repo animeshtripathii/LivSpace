dd725643886912ed4c8cdd1303020bb9e3dec8cb
AnimeshLPU <tripathianimesh38@gmail.com>
2026-08-24 13:34:16 +0530

    fix(css): remove unintended border and radius from .page container by isolating pagination styles to .page-link

--- resources/views/partials/pagination.blade.php
+++ resources/views/partials/pagination.blade.php
@@ -2,31 +2,31 @@
     <nav class="pagination" role="navigation">
         <div class="page-group">
             @if ($paginator->onFirstPage())
-                <span class="page disabled">{{ __('app.pagination.prev') }}</span>
+                <span class="page-link disabled">{{ __('app.pagination.prev') }}</span>
             @else
-                <a class="page" href="{{ $paginator->previousPageUrl() }}" rel="prev">{{ __('app.pagination.prev') }}</a>
+                <a class="page-link" href="{{ $paginator->previousPageUrl() }}" rel="prev">{{ __('app.pagination.prev') }}</a>
             @endif
 
             @foreach ($elements as $element)
                 @if (is_string($element))
-                    <span class="page disabled">{{ $element }}</span>
+                    <span class="page-link disabled">{{ $element }}</span>
                 @endif
 
                 @if (is_array($element))
                     @foreach ($element as $page => $url)
                         @if ($page == $paginator->currentPage())
-                            <span class="page active">{{ $page }}</span>
+                            <span class="page-link active">{{ $page }}</span>
                         @else
-                            <a class="page" href="{{ $url }}">{{ $page }}</a>
+                            <a class="page-link" href="{{ $url }}">{{ $page }}</a>
                         @endif
                     @endforeach
                 @endif
             @endforeach
 
             @if ($paginator->hasMorePages())
-                <a class="page" href="{{ $paginator->nextPageUrl() }}" rel="next">{{ __('app.pagination.next') }}</a>
+                <a class="page-link" href="{{ $paginator->nextPageUrl() }}" rel="next">{{ __('app.pagination.next') }}</a>
             @else
-                <span class="page disabled">{{ __('app.pagination.next') }}</span>
+                <span class="page-link disabled">{{ __('app.pagination.next') }}</span>
             @endif
         </div>
     </nav>
